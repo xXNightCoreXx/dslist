4a0f56a8defa430dbc04c1561b6c90d2cc54d178
igor <ioliveira.io69@gmail.com>
2024-11-16 16:38:33 -0300

    Project creatd

---
+++ src/test/java/com/devcore/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcore.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
